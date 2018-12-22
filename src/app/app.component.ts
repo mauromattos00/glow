db879b1a9c6c2b2bbcb33854446f9f0c17946bb7
Mauro Mattos <mauromattos00@gmail.com>
2018-12-22 12:51:59 -0200

    Dispara ação para recuperar lista de tarefas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as taskListActions from './store/actions/task.actions';
+
+/* NgRx */
+import { Store } from '@ngrx/store';
+import { AppState } from './store/reducers';
+
+/* RXJS */
+import { Observable } from 'rxjs';
+import { Task } from './models/task.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,9 +19,11 @@ export class AppComponent implements OnInit {
 
   public tasks = [];
 
-  constructor() {}
+  constructor(private store: Store<AppState>) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.store.dispatch(new taskListActions.LoadTasks());
+  }
 
   addTaskToList(task: string): void {
     this.tasks.push(task);
